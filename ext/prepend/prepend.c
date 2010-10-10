f6f8e7992260dcda953b361069084d1529b10d2b
John Mair <jrmair@gmail.com>
2010-10-10 22:04:44 +1300

    fixed comments in prepend.c

--- ext/prepend/prepend.c
+++ ext/prepend/prepend.c
@@ -18,22 +18,22 @@ klass_to_s(VALUE self)
 static VALUE
 rb_prepend_module(VALUE klass, VALUE module)
 {
-  /* create module to hold current class's M_TBL */
+  /* create wrapper module to hold current class's M_TBL */
   VALUE class_mod_wrapper = rb_module_new();
 
-  /* clear the default M_TBL already allocated for new the module */
+  /* clear the default M_TBL already allocated for the new wrapper module */
   st_free_table(RCLASS_M_TBL(class_mod_wrapper));
 
-  /* set the module's M_TBL to the current class's */
+  /* set the wrapper module's M_TBL to the current class's */
   RCLASS_M_TBL(class_mod_wrapper) = RCLASS_M_TBL(klass);
 
-  /* store name of current class in module for use by #to_s and #inspect */
+  /* store name of current class in wrapper module for use by #to_s and #inspect */
   rb_iv_set(class_mod_wrapper, "__class_name__", rb_mod_name(klass));
 
   /* set current class's #to_s to return ClassName* */
   rb_define_singleton_method(klass, "to_s", klass_to_s, 0);
 
-  /* set wrapper module #to_s to name of current class */
+  /* set wrapper module's #to_s to name of current class */
   rb_define_singleton_method(class_mod_wrapper, "to_s", class_mod_wrapper_to_s, 0);
 
   /* erase the current class's M_TBL as we're now storing it in the wrapper module */
@@ -42,7 +42,7 @@ rb_prepend_module(VALUE klass, VALUE module)
   /* include the wrapper module first */
   rb_funcall(klass, rb_intern("include"), 1, class_mod_wrapper);
 
-  /* include the module we want to 'prepend' second, so that it's M_TBL gets precedence over M_TBL of original class
+  /* include the module we want to 'prepend' second, so that its M_TBL gets precedence over M_TBL of original class
      (NB: original class's M_TBL is now stored in wrapper module) */
   rb_funcall(klass, rb_intern("include"), 1, module);
   
